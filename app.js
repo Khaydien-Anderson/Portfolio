c36ed35ef718bd97edbec460f0f8fb78114e72d7
K Andy <kdnanderson94@gmail.com>
2021-11-05 18:45:03 +0000

    added JS interactivity within modals inner body + bio added

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ setTimeout(type, 100)
 /////////////// SWIPER JS INITIALIZE ////////////////////////////////
 
 new Glide('.glide', {
-  type: 'slider',
+  type: 'carousel',
   startAt: 0,
   perView: 3,
-  gap: 20,
+  gap: 10,
+  autoplay: 3000,
 breakpoints: {
    768: {
      perView: 2
@@ -34,3 +35,20 @@ breakpoints: {
   }
 }).mount()
 ///////////////////////////////////////////////////////////////////////
+
+/////////////// MODAL INTERACTION ////////////////////////////////
+const projectsUL = document.querySelector('.glide__slides')
+const projects = projectsUL.querySelectorAll('li')
+
+const modalTitle = document.querySelector('.modal-title')
+const modalBody = document.querySelector('.modal-body')
+
+projects.forEach(project => {
+
+  project.addEventListener('click', () => {
+   modalTitle.textContent = project.querySelector('p').textContent
+ modalBody.innerHTML = ''
+ modalBody.innerHTML += `<img class="img-fluid modal-img" style="max-height: 400px;" src="${project.querySelector('img').getAttribute('src')}"></img> <h2>yo</h2>`
+  })
+
+})
